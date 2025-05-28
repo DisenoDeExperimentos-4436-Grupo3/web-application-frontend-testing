eca27b6530dd6f2768498b440f17b1e54e0a7641
John <u202117377@upc.edu.pe>
2025-05-28 16:08:20 -0500

    fix: sitekey captcha

--- src/app/iam/pages/sign-up/sign-up.component.ts
+++ src/app/iam/pages/sign-up/sign-up.component.ts
@@ -53,7 +53,8 @@ export class SignUpComponent extends BaseFormComponent implements OnInit, AfterV
     const waitForCaptchaReady = setInterval(() => {
       if (typeof grecaptcha !== 'undefined' && this.captchaElem) {
         this.captchaId = grecaptcha.render(this.captchaElem.nativeElement, {
-          sitekey: '6Lcb4zUrAAAAAOc0qxROmE9k9Cy3b_vUwBLC9ccs',
+          //sitekey: '6Lcb4zUrAAAAAOc0qxROmE9k9Cy3b_vUwBLC9ccs',
+          sitekey: '6LdhzE0rAAAAAGFtHK0WMKn7wf8M-RsWCKBzwNjh',
           callback: (token: string) => {
             console.log('Captcha token:', token);
           }
