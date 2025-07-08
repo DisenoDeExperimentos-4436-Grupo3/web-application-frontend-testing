406e2d2225d06053cd16af5e08e7ba15789bdc19
JohnArvlo <149823273+JohnArvlo@users.noreply.github.com>
2025-07-08 15:10:41 -0500

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,6 +3,6 @@ export const environment = {production: false,
                             //serverBasePath: "http://localhost:3000"
                             //serverBasePath: "https://my-json-server.typicode.com/estefanojaque/Managewise-reportes"
                             // Server Base Path for Spring Boot REST API
-                            serverBasePath: 'http://localhost:8080/api/v1'
-                            //serverBasePath: 'https://managewise-diseno-back.onrender.com/api/v1'
+                            //serverBasePath: 'http://localhost:8080/api/v1'
+                            serverBasePath: 'https://managewise-diseno-back.onrender.com/api/v1'
                             };
